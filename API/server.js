ced13a6168503b123f4b84e38f30bdb8933195ad
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-04-24 02:09:29 +0100

    change

--- API/server.js
+++ API/server.js
@@ -2,7 +2,7 @@ const { startMongoose } = require('./lib/mongoose');
 // const index = require('./index');
 const http = require('http');
 const app = require('./index');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 
 (async function () {
