2df96dcdba8e6eb854f81df28475a54959a350b7
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-04-27 21:59:04 +0100

    change

--- API/models/getActive.js
+++ API/models/getActive.js
@@ -1,17 +1,6 @@
 const storeActive = require('../schemas/active');
 const run = require('../lib/mongoose');
-// async function getStatus(storeId) {
-// 	try {
-// 		let response = storeActive.findOne(storeId);
 
-// 		if (!response) {
-// 			console.log('user not found');
-// 		}
-// 		return response;
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
 async function getStatus(storeId) {
 	try {
 		let response = await storeActive.findOne(storeId);
