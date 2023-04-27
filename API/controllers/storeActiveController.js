01d86653fb7a7132af4f7384902a19579708da47
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-04-27 11:22:27 +0100

    change

--- API/controllers/storeActiveController.js
+++ API/controllers/storeActiveController.js
@@ -4,11 +4,12 @@ async function getStatusController(req, res) {
 		const findOneQuery = { timestamp_utc: '2023-01-24 09:08:18.436854 UTC' };
 		//let id = ''
 		const store = await getStatus(findOneQuery);
-		if (!store) {
+
+		if (store === null) {
 			res.status(404).json({ error: 'store not found' });
-			return;
+		} else {
+			return store;
 		}
-		return store;
 		console.log(store);
 	} catch (error) {
 		console.log(error);
