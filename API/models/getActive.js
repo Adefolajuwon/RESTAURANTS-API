42cee42f12c208bd029c29184c1a9369935b648c
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-05-23 21:49:34 +0100

    change

--- API/models/getActive.js
+++ API/models/getActive.js
@@ -18,7 +18,7 @@ async function getStatus(storeId) {
 // 	try {
 // 		let response = await storeActive.findOne(store_id);
 // 		if (!response) {
-// 			console.log('stor3ted ndot fssoeund');
+// 			console.log('stor3ted ndot fssccoeund');
 // 		} else {
 // 			return response;
 // 		}
