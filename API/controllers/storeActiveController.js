82e95b300596700c1c59ffab61e7ff2d3ac09ce1
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-06-13 21:12:02 +0100

    change

--- API/controllers/storeActiveController.js
+++ API/controllers/storeActiveController.js
@@ -44,7 +44,7 @@ async function getSchema(req, res) {
 		// if (findOneQuery) {
 		// 	const output = {
 		// 		status: store.status,h
-		// 		timezone: store2.timezone_str,
+		// 		timezone: store2.timezone_str,fd
 		// 		day: store3.day,
 		// 		start: store3.start_time_local,
 		// 		end: store3.end_local,
