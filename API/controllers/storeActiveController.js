09df2cf683ccd65286fe7f6efe84434a51bf15b2
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-06-15 12:36:22 +0100

    change

--- API/controllers/storeActiveController.js
+++ API/controllers/storeActiveController.js
@@ -28,40 +28,24 @@ async function getSchema(req, res) {
 			return; // Return early to prevent further execution
 		}
 
-		// const findOneQuery2 = { store_id: '5415949628544298000' };
 		const store2 = await getStorebyIdtimezone(findOneQuery);
 		if (!store2) {
 			res.status(500).json({ error: 'store 2 not found' });
 			return; // Return early to prevent further execution
 		}
-		// const findOneQuery3 = { store_id: '5955337179846162432' };
 		const store3 = await getStorebyIdbusinesshour(findOneQuery);
 		if (!store) {
 			res.status(404).json({ error: 'store 1 not found' });
 			return; // Return early to prevent further execution
 		}
 
-		// if (findOneQuery) {
-		// 	const output = {
-		// 		status: store.status,
-		// 		timezone: store2.timezone_str,fd
-		// 		day: store3.day,
-		// 		start: store3.start_time_local,
-		// 		end: store3.end_local,
-		// 	};
-		// 	res.status(200).json(output); // Send the output as JSON response
-		// } else {
-		// 	res.status(401).json({ error: 'store not found' }); // Send the output as JSON response
-		// }
-
-		// console.log(output);
 		if (findOneQuery) {
 			const output = {
 				status: store.status,
 				timezone: store2.timezone_str,
 				day: store3.day,
 				start: store3.start_time_local,
-				end: store3.end_local,
+				end: store3.end_time_local,
 			};
 			res.status(200).json(output); // Send the output as JSON response
 			console.log(output); // Log the output
