541aa0f29100a6fb5fea896479edc5a6345683fd
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-06-15 13:27:24 +0100

    change

--- API/controllers/storeActiveController.js
+++ API/controllers/storeActiveController.js
@@ -48,7 +48,7 @@ async function getSchema(req, res) {
 				end: store3.end_time_local,
 			};
 			res.status(200).json(output); // Send the output as JSON response
-			console.log(output); // Log the output
+			console.log(output); // Log the outputf
 		} else {
 			res.status(401).json({ error: 'store not found' }); // Send the output as JSON response
 		}
