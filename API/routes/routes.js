5cc35e9b8991f64a1758bdea20f0e26e75d49234
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-07-05 21:04:17 +0100

    change

--- API/routes/routes.js
+++ API/routes/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const express = require('express');
-const { createReport } = require('../controllers/report.controller');
+const {
+	createReport,
+	reportOutput,
+} = require('../controllers/report.controller');
 
 const router = express.Router();
 
 router.post('/trigger_report', createReport);
+router.get('/get_report', reportOutput);
 // router.get('/get_report', async (req, res) => {
 // 	const reportId = req.query.report_id;
 // 	// Check the status of the report generation based on the report ID
